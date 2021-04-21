3b9d25db73c6c5e1942baf4aecb0a77a2e88ea61
Rebecca Chen <beining0026@gmail.com.au>
2021-04-21 13:25:03 +1000

    requested changes

--- file_upload_handler/src/server.js
+++ file_upload_handler/src/server.js
@@ -1,67 +1,59 @@
-import express from 'express'
-import multer from 'multer'
+import express from "express";
+import multer from "multer";
 
-const app =express();
+const PORT = process.env.PORT || 8000
+const app = express();
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniquepreffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null,uniquepreffix + "-"+ file.originalname )
-    }
-  })
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniquepreffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniquepreffix + "-" + file.originalname);
+  },
+});
 
-  function fileFilter (req, file, cb) {
-
-    // The function should call `cb` with a boolean
-    // to indicate if the file should be accepted
-    const acceptFileType = ['video/mp4','image/png','image/jpeg'  ,'image/gif']
-    if (acceptFileType.includes(file.mimetype )){
-        // To accept the file pass `true`, like so:
-        cb(null, true)
-    }else{
-        cb(new Error("invalid File type\nonly accpet" + acceptFileType ))
-    }
-    
-    //cb(null, false)
-  
-    
-  
-    // You can always pass an error if something goes wrong:
-    //cb(new Error('I don\'t have a clue!'))
-  
+function fileFilter(req, file, cb) {
+  // The function should call `cb` with a boolean
+  // to indicate if the file should be accepted
+  const acceptFileType = ["video/mp4", "image/png", "image/jpeg", "image/gif"];
+  if (acceptFileType.includes(file.mimetype)) {
+    // To accept the file pass `true`, like so:
+    cb(null, true);
+  } else {
+    cb(new Error("invalid File type\nonly accept" + acceptFileType));
   }
-  
-  var upload = multer({ storage: storage , 
-    limits:{
-      fileSize : 1024 * 1024 * 5, // 5MB
-        },
-        fileFilter : fileFilter})
 
+  //cb(null, false)
 
-app.get("/test_get", (req, res)=>{
-    res.send("test_get");
-})
+  // You can always pass an error if something goes wrong:
+  //cb(new Error('I don\'t have a clue!'))
+}
 
-app.post("/fileReceiver",upload.single('userFile'), (req, res)=>{
-    
+var upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // 5MB
+  },
+  fileFilter: fileFilter,
+});
 
-    console.log(req.file)
-    console.log(req.body)
-    res.send(req.file)
-})
+// app.get("/test_get", (req, res)=>{
+//     res.send("test_get");
+// })
 
-app.post('/multiFileReceiver', upload.array("userFile", 10), (req, res)=>{
-    
-    res.send("Multiple File received");
-    console.log(req)
+app.post("/fileReceiver", upload.single("userFile"), (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+  res.send(req.file);
+});
 
-   
-})
+app.post("/multiFileReceiver", upload.array("userFile", 10), (req, res) => {
+  res.send("Multiple File received");
+  console.log(req);
+});
 
-
-app.listen(8000, ()=>{
-    console.log("listening on port 8000")
-})
+app.listen(PORT, () => {
+  console.log("listening on port 8000");
+});
