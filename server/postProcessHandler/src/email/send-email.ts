137ece955d14e83d599154afc6172c77dfe97a36
alexdelicato <adel0011@student.monash.edu>
2021-08-24 12:51:57 +1000

    updated script ready for inputs

--- server/postProcessHandler/src/email/send-email.ts
+++ server/postProcessHandler/src/email/send-email.ts
@@ -1,23 +1,15 @@
 /* 
-script to be called by post process handler when job is complete
+Script for sending email to user of job results
 code referencing: https://www.cloudmailin.com/blog/sending_and_receiving_email_in_node_2021 
-
-TODO:
-- link to post process handler so called when job is complete
-- get user email from DB (link DB)
-- get report link to send
-- need to verify account without including password as repo is open source
 
 */
 
-import nodemailer from "nodemailer";
-import Mail from "nodemailer/lib/mailer";
+import {credentials} from './credentials';
 
-async function main() {
+async function sendEmail(emailString: string, htmlResult: string) {
+    
+    const nodemailer = require("nodemailer");
     const hostname = "smtp.gmail.com";  // hostname to connect to
-    const sender_email = "vtaa.report.response@gmail.com";  // team email (sender)
-    const password = String(prompt("enter password: "));    // ask for password
-    const receiver_email = "adel0011@student.monash.edu"
 
 
     const transporter = nodemailer.createTransport({
@@ -26,13 +18,15 @@ async function main() {
         secure: true,
         requireTLS: true,
         auth: {
-            user: sender_email,
-            pass: password,
+            user: credentials.email,
+            pass: credentials.password,
         },
         logger: true
     });
 
     // verify connection
+    console.log('verifying transporter...')
+    /*
     transporter.verify(function (error, success) {
         if (error) {
         console.log(error);
@@ -40,16 +34,21 @@ async function main() {
         console.log("Server is ready to take our messages");
         }
     });
+    */
   
+   console.log('sending email...')
     // send email with define transport object
     // currently test email
     const info = await transporter.sendMail({
-        from: sender_email,
-        to: receiver_email,
-        subject: "Test Email",
-        text: " this is test email from VTAA",
-        html: "<strong> test html </strong>",
-    });
+        from: credentials.email,
+        to: emailString,
+        subject: "Vision Job Results",
+        text: "",
+        html: htmlResult,
+    }); // add callback to get response
 
+    // log response
     console.log("message sent: %s", info.response);
+
+    // inform front end email has been sent
 }
