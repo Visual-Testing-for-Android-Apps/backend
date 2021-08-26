6673e0661d6cd12332e00aa38d351a59f99aff2f
wvan0005 <wvan0005@student.monash.edu>
2021-08-26 19:37:39 +1000

    Added database job object processing to main function

--- server/reportGenerator/src/reportGen.ts
+++ server/reportGenerator/src/reportGen.ts
@@ -1,8 +1,15 @@
-function generate_vid_report(index: number, algResult: number) {
-    // Input: index of vid in batch, number outputted from Seenomally alg
-    // Output: html string describing identified UI issue
+import { SQSEvent } from "aws-lambda";
+import { getItem, GetItemInput } from "./service/dynamodbClient";
 
-    let titles: string[] = [
+/**
+ * Generates a html report for a given image issue
+ * 
+ * @param index - index of original video in batch
+ * @param algResult - issue type code
+ * @returns html string containing image, heatmap, and description
+ */
+function generateVidReport(index: number, algResult: number): string {
+    const titles: string[] = [
         "Cannot place image in space",
         "Pass through other material",
         "Lack of scrimmed background",
@@ -14,21 +21,21 @@ function generate_vid_report(index: number, algResult: number) {
         "Lack of shadow",
         "Invisible scrim of modal bottom sheet"
     ]
-    let desc: string[] = [
+    const desc: string[] = [
         "The video could not be matched in the sample space. No known design violations detected.",
         "Text and/or images is found to pass through other page elements resulting in some content being hidden from view.",
         "No scrimmed background is detected which can result in text that is difficult to read.",
         "A snackbar is found to block the app bar at the bottom of the screen which prevents easy page navigation until the snackbar disappears.",
         "Multiple banners were found to stack on each other hiding some information from the user.",
-        "A card is found to use the flip annimation.", // ADD MORE
-        "", // ONE CARD BEHIND OTHER
+        "A card uses the flip annimation which forces the user to wait for the animation to end before app use can continue.",
+        "A card was detected to be behind another card hiding some information from the user.",
         "Multiple snackbars were found to stack on each other hiding some information from the user.",
-        "The background lacks shadow when another window appears which can take away the users attention.",
-        "", // SCRIM
+        "The background lacks shadow when another window appears. This can divert the user's attention away from the window.",
+        "UI elements behind a modal bottom sheet lack a visible scrim/filter whilst the bottom sheet is onscreen.  A visible scrim indicates to the user that these elements cannot be interacted with whilst the menu is displayed.",
     ]
 
-    let res = "<h2>Item " + (index) + "</h2>";
-    res += "<p>" + titles[algResult] + ".<br>" + desc[algResult] + "</p>";
+    let res = "<h2>Item " + index.toString() + "</h2>";
+    res += "<p>" + titles[algResult] + "<br>" + desc[algResult] + "</p>";
     return res;
 }
 
@@ -42,47 +49,65 @@ function generate_vid_report(index: number, algResult: number) {
  * @param algResult - issue type code
  * @returns html string containing image, heatmap, and description
  */
-function generate_img_report(index: number, filePath: string, algResultPath: string, algResult: number) {
-    let titles: string[] = [
-        "General issue heatmap",  
+function generateImgReport(index: number, filePath: string, algResultPath: string, algResult: number): string {
+    const titles: string[] = [
+        "General issue heatmap",
         "Null value",
         "Missing image",
         "Component occlusion"
         // no models for blurred screen or text overlap, so no description is needed
     ]
-    let desc: string[] = [
-        "Heatmap highlights all potential issues",
-        "NULL text is being displayed, instead of the correct information",
-        "A placeholder 'missing/broken image' symbol is displayed, instead of an intended image",
-        "Text is overlapped or obscured by other components"
+    const desc: string[] = [
+        "Heatmap highlights all potential issues.",
+        "'NULL' text is being displayed, instead of the correct information.",
+        "A placeholder 'missing/broken image' symbol is displayed, instead of an intended image.",
+        "Text is overlapped or obscured by other components."
     ]
 
-    let res = "<h2>Item " + (index) + "</h2>";
+    let res = "<h2>Item " + index.toString() + "</h2>";
     res += "<image src='" + filePath + "'>";
     res += "<image src='" + algResultPath + "'>";
     res += "<p>" + titles[algResult] + ".<br>" + desc[algResult] + "</p>";
     return res;
 }
 
-function main(jsonString: string) {
-    // Input: job object as jsonString
-    // Output: html string describing issues identified for each img/vid
-    
-    let res = "";
-    let jsonArray = JSON.parse(jsonString);
-    let files = jsonArray.files;
-    let results = jsonArray.results;
-
-    for (let i = 0; i < results.length; i++) {
-        let file = files[i];
-        let result = results[i];
-
-        if (file.fileType == "image") {
-            res += generate_img_report(i, file.fileReferenceInS3Bucket, result.resultFileReference, result.resultCode);
-        } else if (file.fileType == "video") {
-            res += generate_vid_report(i, result.resultCode);
+/**
+ * 
+ * @param event 
+ * @param context 
+ * @returns html string describing issues identified for each img/vid
+ */
+export const generateReport = async (event: SQSEvent, context: AWSLambda.Context): Promise<any> => {
+    const key: string = JSON.parse(event.Records[0].body).jobKey;
+    const request: GetItemInput = {
+        TableName: 'JobTable',
+        Key: {
+            batch_id: { S: key }
         }
-        res += "<p><br><br></p>";
+    };
+    const dbRes = await getItem(request);
+
+    let res = "";
+    if (dbRes.Item != null) {
+        const files = dbRes.Item.files;
+        if (files.L != null) {
+            files.L.forEach((element, index) => {
+                if (element.M != null) {
+                    const fileRef = element.M.fileRef.S;
+                    const fileType = element.M.fileType.S;
+                    const resultCode = element.M.resultCode.N;
+                    const resultFileRef = element.M.resultFileReference.S;
+
+                    if (fileType === "image" && !(fileRef == null || resultFileRef == null || resultCode == null)) {
+                        res += generateImgReport(index, fileRef, resultFileRef, +resultCode);
+                    } else if (fileType === "video" && !(resultCode == null)) {
+                        res += generateVidReport(index, +resultCode);
+                    }
+                    res += "<p><br><br></p>";
+                }
+            });
+        }
     }
     return res;
-}
+    // Call Collins' function?
+};
