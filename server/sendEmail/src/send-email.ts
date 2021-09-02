04bcb945cd7d48b15becfa674d8f3d2c7b3a8ac9
alexdelicato <adel0011@student.monash.edu>
2021-09-02 22:39:58 +1000

    removed uneccessary comments before pull request

--- server/sendEmail/src/send-email.ts
+++ server/sendEmail/src/send-email.ts
@@ -7,85 +7,84 @@ i.e. sendMail(<string>, <string>).catch(console.error)
 
 */
 
-import { SQSEvent } from 'aws-lambda'
-import { getItem, GetItemInput } from './service/dynamodbClient'
-import { AttributeMap } from 'aws-sdk/clients/dynamodb'
-import dotenv from 'dotenv'
-import nodemailer from 'nodemailer'
-// const nodemailer = require("nodemailer");   // import nodemailer to use
-dotenv.config()
+import { SQSEvent } from "aws-lambda";
+import { getItem, GetItemInput } from "./service/dynamodbClient";
+import { AttributeMap } from "aws-sdk/clients/dynamodb";
+import dotenv from "dotenv";
+let nodemailer = require("nodemailer"); // import nodemailer to use
+dotenv.config();
 
 export const awaitJob = async (request: GetItemInput): Promise<AttributeMap | null> => {
-  const res = await getItem(request)
-  if (res.Item != null) {
-    return res.Item
-  }
-  return null
-}
+	const res = await getItem(request);
+	if (res.Item != null) {
+		return res.Item;
+	}
+	return null;
+};
 
 export const sendEmail = async (event: SQSEvent, context: AWSLambda.Context): Promise<any> => {
-  const hostname = 'smtp.gmail.com' // hostname to connect to
+	const hostname = "smtp.gmail.com"; // hostname to connect to
 
-  const key: string = JSON.parse(event.Records[0].body).jobKey // get job key from DB
-  console.log('Job key: ' + String(key))
+	const key: string = JSON.parse(event.Records[0].body).jobKey; // get job key from DB
+	console.log("Job key: " + String(key));
 
-  // make request for DB info using job key, store in request
-  const request: GetItemInput = {
-    TableName: process.env.JOB_TABLE as string,
-    Key: {
-      id: { S: key }
-    }
-  }
-  const res = await awaitJob(request) // wait for job
+	// make request for DB info using job key, store in request
+	const request: GetItemInput = {
+		TableName: process.env.JOB_TABLE as string,
+		Key: {
+			id: { S: key },
+		},
+	};
+	const res = await awaitJob(request); // wait for job
 
-  // check if result exists with email and link, will add: && res?.link?.S
-  if (res?.email?.S != null) {
-    // get email and link from job
-    const emailString = res.email.S
-    const htmlResult = res.link.S
+	// check if result exists with email and link, will add: && res?.link?.S
+	if (res?.email?.S != null) {
+		// get email and link from job
+		const emailString = res.email.S;
+		const htmlResult = res.link.S;
 
-    // create transport for email
-    const transporter = nodemailer.createTransport({
-      host: hostname,
-      port: 465, // secure -> 465, not secure -> 587
-      secure: true,
-      requireTLS: true,
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-      },
-      logger: true
-    })
+		// create transport for email
+		const transporter = nodemailer.createTransport({
+			host: hostname,
+			port: 465, // secure -> 465, not secure -> 587
+			secure: true,
+			requireTLS: true,
+			auth: {
+				user: process.env.EMAIL,
+				pass: process.env.PASSWORD,
+			},
+			logger: true,
+		});
 
-    // verify connection
-    console.log('verifying transporter...')
-    /*
-        transporter.verify(function (error, success) {
-            if (error) {
-            console.log(error);
-            } else {
-            console.log("Server is ready to take our messages");
-            }
-        });
-        */
+		// verify connection
+		console.log("verifying transporter...");
+		/*
+    transporter.verify(function (error, success) {
+        if (error) {
+        console.log(error);
+        } else {
+        console.log("Server is ready to take our messages");
+        }
+    });
+    */
 
-    console.log('sending email...')
-    // send email with defined transport object
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL,
-      to: emailString,
-      subject: 'Vision Job Results',
-      text: '',
-      html: htmlResult
-    }) // add callback to get response
+		console.log("sending email...");
+		// send email with defined transport object
+		const info = await transporter.sendMail({
+			from: process.env.EMAIL,
+			to: emailString,
+			subject: "Vision Job Results",
+			text: "",
+			html: htmlResult,
+		});
 
-    // log response
-    console.log('message sent: %s', info.response)
-  } else {
-    return null
-  }
-}
+		// log response
+		console.log("message sent: %s", info.response);
+	} else {
+		return null;
+	}
+};
 
 exports.handler = async (event: SQSEvent, context: AWSLambda.Context): Promise<any> => {
-  return await sendEmail(event, context)
-}
+	return await sendEmail(event, context);
+};
