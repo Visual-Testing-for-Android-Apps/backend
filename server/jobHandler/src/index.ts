b55ef2ea247129997cb5ee49f705221540cc5811
alexdelicato <adel0011@student.monash.edu>
2021-10-07 11:01:58 +1100

    fixed branch & readded job processing email code

--- server/jobHandler/src/index.ts
+++ server/jobHandler/src/index.ts
@@ -5,6 +5,8 @@ import { getJob, updateJobStatus } from "./service/dynamodbService"
 import { JobStatus } from "./service/jobModel"
 import { modelTrigger } from "./service/modelTrigger"
 import { selfEnvoke } from "./service/sqsClient.js"
+import { sendProcessingEmail } from "./jobProcessingEmail";
+
 
 //Exports isJobComplete for use with AWS lambda
 export const handler = async (event: SQSEvent, context: AWSLambda.Context): Promise<void> => {
@@ -52,6 +54,12 @@ const jobHandler = async (context: AWSLambda.Context, key: string): Promise<{ is
 	}
 	//Set the job status to processing, 
 	await updateJobStatus(key, JobStatus.PROCESSING)
+	// send email telling user job is processing
+	try {
+		await sendProcessingEmail(key);
+	} catch (error) {
+		throw Error(`failed to send processing email for job:${job.id}`);
+	}
 	await modelTrigger(context, job)
 	//The actual checking if all jobs are complete could be redundant, but leaving it in doesn't hurt anything
 	return { isCompleted: await isJobComplete(key) }
