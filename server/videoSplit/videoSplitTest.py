54bcd8d5ea2629cf1e68fe8320f38337e8f9275c
josh-sharp <81729266+josh-sharp@users.noreply.github.com>
2021-10-10 18:39:24 +1100

    Test driver file

---
+++ server/videoSplit/videoSplitTest.py
@@ -0,0 +1,7 @@
+import videoSplitConcept
+
+
+videoSplitConcept.video_split("./server/videoSplit/testVideos/test1.mp4")
+# videoSplitConcept.video_split("./server/videoSplit/testVideos/test2.mp4")
+# videoSplitConcept.video_split("./server/videoSplit/testVideos/test3.mp4")
+
